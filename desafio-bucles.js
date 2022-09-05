0919ee1002e26f76a59a852b6444f7e1d89f06a1
bri-ramirez <briggitte.ramirez13@gmail.com>
2022-09-05 16:42:14 -0300

    se agrega un while

--- desafio-bucles.js
+++ desafio-bucles.js
@@ -7,10 +7,12 @@
  * imprimir los numeros impares
  */
 console.log('\nNúmeros impares');
- for(var i = 1 ;  i <= 20 ; i++){
-    if(i%2 !== 0){
+var i = 1;
+while(i <= 20){
+    if(i%2 !== 0 ){
         console.log(i);
     }
+    i++;
 }
 
 /**
